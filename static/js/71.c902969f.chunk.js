f77979d9f4f0f5bdfb780c0691ce71fee1031a39
anigvo <116902287+anigvo@users.noreply.github.com>
2023-10-23 15:28:21 +0000

    Deploying to gh-pages from @ anigvo/car-rental@d6926b6c23ca5cf7cc974f8059167a006831f562 🚀

---
+++ static/js/71.c902969f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[71],{71:function(t,e,a){a.d(e,{bjh:function(){return n}});var r=a(9983);function n(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M289.94 256l95-95A24 24 0 00351 127l-95 95-95-95a24 24 0 00-34 34l95 95-95 95a24 24 0 1034 34l95-95 95 95a24 24 0 0034-34z"}}]})(t)}}}]);
+//# sourceMappingURL=71.c902969f.chunk.js.map
